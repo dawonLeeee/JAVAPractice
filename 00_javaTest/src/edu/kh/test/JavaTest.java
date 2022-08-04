eda0d333ddaf6aafb25a69a17d29f27799c38c99
user1 <user1@DESKTOP-IJ8C807>
2022-08-04 14:45:30 +0900

    first commit

--- 00_javaTest/src/edu/kh/test/JavaTest.java
+++ 00_javaTest/src/edu/kh/test/JavaTest.java
@@ -4,6 +4,7 @@ package edu.kh.test;
 
 public class JavaTest {
 	
+	//main메서드 : 자바 어플리케이션을 실행하기 위해 반드시 필요한 메서드
 	public static void main(String[] args) {
 		// System.out.println("Hello World");
 		System.out.println("println()은 한줄출력입니다");
