2611edc376bafad8bb3f77ec28f53f7ad7135220
user1 <user1@DESKTOP-IJ8C807>
2022-08-04 14:43:09 +0900

    first commit

---
+++ 00_javaTest/src/edu/kh/test/JavaTest.java
@@ -0,0 +1,13 @@
+package edu.kh.test;
+
+// 한 줄 주석(컴파일러(번역기)가 해석하지 않는 부분)
+
+public class JavaTest {
+	
+	public static void main(String[] args) {
+		// System.out.println("Hello World");
+		System.out.println("println()은 한줄출력입니다");
+		System.out.println(1368+4315);
+	}
+
+}
